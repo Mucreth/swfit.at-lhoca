b3b114c8f85e6496e52dc5fcdac48c5d6eef5bb0
Nusret Uzman <nusretuzman@Nusrets-MacBook-Air.local>
2023-01-02 11:49:27 +0300

    m

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//variables
+
+var userName = "nusret"
+
+var userSurname = "uzman"
+
+let age = 34.0
+var height = 1.78
+
+age * height
+
+//--------- PART 2 ---------
+
+var myString : String = "nusret"
+let anotherNumber : Int = 45
+
+var stringNumber : String = String(40)
+
+let testVariable = "test"
+testVariable.uppercased()
+var upperTest = testVariable.uppercased()
+print(upperTest)
+
+
+// -------- PART 3 ----------
+
+var myFavoriteMovie = ["eragon", "lotr", "hp"]
+
+
