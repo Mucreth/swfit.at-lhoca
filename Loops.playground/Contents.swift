bf07572ab0bc0ffaec0161d12936157ce0e35943
mucreth <nusretuzman@Nusrets-MacBook-Air.local>
2023-01-04 20:40:04 +0300

    m

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+
+//while
+
+var i = 2
+
+while i <= 20 {
+    //print(i)
+    i += 1
+}
+3 < 6
+
+//for loop
+
+var myFruitArray = ["banana", "melon", "kiwi"]
+
+for fruit in myFruitArray{
+    print(fruit)
+}
+
+var mynt = [10,20,30,40,50]
+
+for say in mynt{
+    print(say / 5)
+}
+
+// if
+
+var myAge = 29
+
+if myAge > 30{
+    print(myAge)
+} else {
+    print("errror")
+}
